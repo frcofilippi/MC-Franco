c452e9da72e31c26bc03d6028e0c4293d0f23a34
Franco Filippi <frcofilippi@gmail.com>
2020-11-19 15:02:06 -0300

    Updates

---
+++ precache-manifest.3e0f5d35a28f6c9cd4c618a0f9a30053.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522b341de4a62b3a9d0f796f8d6efb5c",
+    "url": "/MC-Franco/index.html"
+  },
+  {
+    "revision": "474dc10ed36630f3708b",
+    "url": "/MC-Franco/static/css/3.157a16db.chunk.css"
+  },
+  {
+    "revision": "1457ecc61e14188bd1e8",
+    "url": "/MC-Franco/static/css/main.212b0db5.chunk.css"
+  },
+  {
+    "revision": "5f56b5c2932982f59558",
+    "url": "/MC-Franco/static/js/2.8f68ffc8.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/MC-Franco/static/js/2.8f68ffc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474dc10ed36630f3708b",
+    "url": "/MC-Franco/static/js/3.5584ad0f.chunk.js"
+  },
+  {
+    "revision": "1457ecc61e14188bd1e8",
+    "url": "/MC-Franco/static/js/main.072d95ea.chunk.js"
+  },
+  {
+    "revision": "b2469dc399e47738a6be",
+    "url": "/MC-Franco/static/js/runtime-main.ba04495d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/MC-Franco/static/media/burger-logo.b8503d26.png"
+  }
+]);
